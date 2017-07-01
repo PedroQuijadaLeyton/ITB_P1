6e43c212d17d3fe40820f6e4a3d6157cbe745b9b
PedroQuijadaLeyton <pedro@quiley.com>
2017-07-01 12:30:34 +0000

    init

---
+++ Assets/Script/Click.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Click : MonoBehaviour
+{
+
+    Manager m;
+    Color transparent;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m = FindObjectOfType<Manager>();
+        transparent = new Color(1, 1, 1, 0.3f);
+
+    }
+
+    public void correct()
+    {
+        m.good.Play();
+        GetComponent<Image>().color = transparent;
+        transform.GetChild(0).gameObject.SetActive(true);
+        GetComponent<CanvasGroup>().interactable = false;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+    }
+
+    public void incorrect()
+    {
+        m.bad.Play();
+        GetComponent<Animator>().SetBool("start", true);
+        StartCoroutine(waitforsec());
+    }
+
+    IEnumerator waitforsec()
+    {
+        yield return new WaitForSeconds(0.3f);
+        GetComponent<Animator>().SetBool("start", false);
+    }
+
+}
---
+++ Assets/Script/Manager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+
+    public AudioSource good;
+    public AudioSource bad;
+
+	// Use this for initialization
+	void Start ()
+    {
+        bad = GetComponents<AudioSource>()[0];
+        good = GetComponents<AudioSource>()[1];
+    }
+}
